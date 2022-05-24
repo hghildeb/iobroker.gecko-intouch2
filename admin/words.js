4d385d2059947fd9ce3994a343014b2fdb2f1530
hghildeb <hh@henning-hildebrand.de>
2022-05-24 22:41:11 +0200

    Initial

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'gecko-intouch2 adapter settings': {
+        'en': 'Adapter settings for gecko-intouch2',
+        'de': 'Adaptereinstellungen für gecko-intouch2',
+        'ru': 'Настройки адаптера для gecko-intouch2',
+        'pt': 'Configurações do adaptador para gecko-intouch2',
+        'nl': 'Adapterinstellingen voor gecko-intouch2',
+        'fr': "Paramètres d'adaptateur pour gecko-intouch2",
+        'it': "Impostazioni dell'adattatore per gecko-intouch2",
+        'es': 'Ajustes del adaptador para gecko-intouch2',
+        'pl': 'Ustawienia adaptera dla gecko-intouch2',
+        'zh-cn': 'gecko-intouch2的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'вариант 2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};
